b638bac7846ce98f5a0aa033a152c54f8797e24e
Jesus Gollonet <jesusgollonet@gmail.com>
2013-06-23 01:13:57 +0200

    added info to env_helper

--- helpers/env_helper.rb
+++ helpers/env_helper.rb
@@ -1,5 +1,8 @@
+# sensible settings are stored in a .gitignored file called local_env.yml
+# and read as ENV variables below
+# http://railsapps.github.io/rails-environment-variables.html
+
 module EnvHelper
-  
   def EnvHelper.test
     puts 'works!'
     puts File.exists? File.join(File.dirname(__FILE__) , '..', 'local_env.yml')
